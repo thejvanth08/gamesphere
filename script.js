3c445ed724e126c7f4181946908ce7ce2ea1f6ca
Thejvanth <thejvanth08@gmail.com>
2023-08-25 18:01:49 +0530

    Code Responsive styles

--- script.js
+++ script.js
@@ -15,3 +15,33 @@ audioImg.addEventListener("mouseover", () => {
 audioImg.addEventListener("mouseout", () => {
   audioImg.src = audioPrev;
 });
+
+photoImg.addEventListener("mouseover", () => {
+  photoPrev = photoImg.src;
+  //   using relative path from root of the website/domain/server
+  photoImg.src = "/assets/photo-22.webp";
+});
+
+photoImg.addEventListener("mouseout", () => {
+  photoImg.src = photoPrev;
+});
+
+videoImg.addEventListener("mouseover", () => {
+  videoPrev = videoImg.src;
+  //   using relative path from root of the website/domain/server
+  videoImg.src = "/assets/video-33.webp";
+});
+
+videoImg.addEventListener("mouseout", () => {
+  videoImg.src = videoPrev;
+});
+
+miniImg.addEventListener("mouseover", () => {
+  miniPrev = miniImg.src;
+  //   using relative path from root of the website/domain/server
+  miniImg.src = "/assets/mini-11.webp";
+});
+
+miniImg.addEventListener("mouseout", () => {
+  miniImg.src = miniPrev;
+});
